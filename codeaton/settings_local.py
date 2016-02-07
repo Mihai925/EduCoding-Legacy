46debda94412fcc4ef12b619e186a7140c19ff6f
Mihai Jiplea <mihai@jiplea.com>
2016-02-07 00:16:25 +0000

    Adding SQLite for dev

--- codeaton/settings_local.py
+++ codeaton/settings_local.py
@@ -7,12 +7,8 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'educoding',
-        'USER': 'educoding',
-        'PASSWORD': 'educoding',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
