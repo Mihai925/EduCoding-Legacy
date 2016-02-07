a949c81ccae8e5741382ebb75d2fbdddae6dcf2a
Mihai Jiplea <mihai@jiplea.com>
2016-02-07 18:01:10 +0000

    Replacing multiple settings with environment variables

--- educoding/settings.py
+++ educoding/settings.py
@@ -1,9 +1,194 @@
-__author__ = 'varun'
+import os
 
-from settings_common import *
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-from settings_local import *
 
-MEDIA_ROOT = 'replace_with_path/'
-MEDIA_URL = 'replace_with_path/'
-SOUTH_TESTS_MIGRATE = False 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY','')
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Custom apps
+    'Exercise',
+    'Compiler',
+    'Class',
+    "Utils",
+    'django_ace',
+    'selectable',
+    'schedule',
+    'LandingPage',
+    'UserProfile',
+    'Autotester'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'educoding.urls'
+
+WSGI_APPLICATION = 'educoding.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'), )
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader')
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    'django.core.context_processors.csrf',
+    "django.core.context_processors.request",
+)
+
+AUTH_PROFILE_MODULE = "model.models.UserProfile"
+
+
+
+LOGIN_URL = "/"
+LOGOUT_URL = "/authentication/logout"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME',''),
+        'USER': os.environ.get('DB_USER',''),
+        'PASSWORD': os.environ.get('DB_PASSWORD',''),
+        'HOST': os.environ.get('DB_HOST',''),
+        'PORT': os.environ.get('DB_PORT',''),
+    }
+}
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.dirname(os.path.abspath(__file__)) + "/../logs/log.log",
+            'maxBytes': 5000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+
+
+
+URL = os.environ.get('DEBUG','')
+
+COMPILER_API = os.environ.get('COMPILER_API','')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG','')
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+SOUTH_TESTS_MIGRATE = False
+
+#Things I need to get rid of (currently needed for legacy resons):
+
+SUBCRIPTION_SPREADSHEET_KEY = 'mock'
+SUBCRIPTION_WORKSHEET_KEY = 'mock'
+SMTP_SERVER = "mock"
+SMTP_SERVER_PORT = 587
+SMTP_USERNAME = "mock"
+SMTP_PASSWORD = "mock"
+SMTP_FROM_EMAIL = "mock"
+GOOGLE_DRIVE_USERNAME = "mock"
+GOOGLE_DRIVE_PASSWORD = "mock"
+GOOGLE_DRIVE_FOLDER_KEY = 'mock'
+HELLO_EMAIL = "mock"
+GOOGLE_DRIVE_USERNAME = "mock"
+GOOGLE_DRIVE_PASSWORD = "mock"
+GOOGLE_DRIVE_FOLDER_KEY = 'mock'
+HELLO_EMAIL = "mock"
--- educoding/urls.py
+++ educoding/urls.py
@@ -1,21 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import RedirectView
 from authentication import urls as auth_urls
 from Class import urls as class_urls
 from Exercise import urls as exercise_urls
 from LandingPage import urls as landing_urls
-from .settings import USE_LANDING_PAGE
 from UserProfile import urls as profile_urls
 from settings import MEDIA_ROOT, MEDIA_URL
 from django.conf.urls.static import static
 
 
-home_page_pattern = []
-if USE_LANDING_PAGE:
-    home_page_pattern.append(url(r'^$', include(landing_urls.urlpatterns)))
-else:
-    home_page_pattern.append(url(r'^$', RedirectView.as_view(url='/authentication/')))
+home_page_pattern = [url(r'^$', include(landing_urls.urlpatterns))]
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
