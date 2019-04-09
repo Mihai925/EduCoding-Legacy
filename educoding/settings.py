ce81c3830b5c30c2db2b7c29e96cb26c5cf4f8e9
Mihai Jiplea <mihai@jiplea.com>
2019-04-08 20:50:11 -0700

    Disabling logging for now, will enable later

--- educoding/settings.py
+++ educoding/settings.py
@@ -127,53 +127,6 @@ DATABASES = {
     }
 }
 
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'standard': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S"
-        },
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'logfile': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.dirname(os.path.abspath(__file__)) + "/../logs/log.log",
-            'maxBytes': 5000,
-            'backupCount': 2,
-            'formatter': 'standard',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'standard'
-        },
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console', 'logfile'],
-            'level': 'DEBUG',
-        },
-        'django': {
-            'handlers': ['console'],
-            'propagate': True,
-            'level': 'WARN',
-        },
-        'django.db.backends': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
-
 #E-mail setup for SES:
 
 COMPILER_API = os.environ.get('COMPILER_API', '')
